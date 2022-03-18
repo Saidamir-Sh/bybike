bb4d5ab51f167c78825323f05df9f7c3fc023470
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-03-18 06:07:26 +0100

    dashboart intro

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -2,21 +2,36 @@ import React, {useState} from 'react'
 import '../styles/Dashboard.css'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import dashboardImage from '../assets/dashboardimg.png'
 
 function Dashboard() {
 
+    // show hide sidebar
     const [isActive, setIsActive] = useState(false)
+
+    // search input results
+    const [searchQuery, setSearchQuery] = useState('')
+    const [showResults, setShowResults] = useState(false)
 
     // collapse side bar
     const handleSideBar = (e) => {
     e.preventDefault()
     setIsActive(!isActive)
     }
+
   return (
     <div id='dashboard' className={!isActive ? 'dashboard' : 'dashboard inactive'}>
-         <div onClick={(e) => handleSideBar(e)} className=' dashboard-arrow d-flex align-items-center justify-content-center'>
-            {isActive ? <ArrowRightIcon /> : <ArrowLeftIcon/>}
-          </div>
+      <div className='dashboard-intro'>
+        <img src={dashboardImage} width='50%' alt='Logo for Dashboard' />
+        <div className='pulse-img circle-img'></div>
+      </div>
+      <div className='intro-text'>
+        <h1>ByBike</h1>
+        <p>Short rides to your work or appointments, or explore the city. It's easy with ByBike</p>
+      </div> 
+      <div onClick={(e) => handleSideBar(e)} className=' dashboard-arrow d-flex align-items-center justify-content-center'>
+        {isActive ? <ArrowLeftIcon/> : <ArrowRightIcon/>}
+      </div>
     </div>
   )
 }
