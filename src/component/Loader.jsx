5541cad82c179c095fc3277ea55ec8e7663e0a23
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-04-05 07:45:04 +0200

    added intro text into loader component

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -7,6 +7,10 @@ const Loader = () => {
     <div className='d-flex align-items-center flex-column justify-content-center w-100' style={{height: '100vh'}}> 
         <Hypnosis color="#2b5877" width="200px" height="200px" duration="3s" />
         <img src={logo} className='position-absolute' width={'7%'}/>
+        <div className='position-absolute text-center' style={{bottom: '10%'}}>
+          <h1>ByBike</h1>
+          <p>Short rides to your work or appointments, or explore the city. It's easy with ByBike</p>
+        </div>
     </div>
   )
 }
