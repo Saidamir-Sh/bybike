2c6221c4855634fa4a5161c1e2af6529b45bb89d
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-04-05 05:41:08 +0200

    loading page changed

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Hypnosis } from 'react-cssfx-loading/lib';
-import logo from '../assets/logo.png'
+import logo from '../assets/dashboardimg.png'
 
 const Loader = () => {
   return (
     <div className='d-flex align-items-center flex-column justify-content-center w-100' style={{height: '100vh'}}> 
-        <Hypnosis color="#2b5877" width="100px" height="100px" duration="3s" />
-        <img src={logo} className='fixed-bottom mx-auto pb-2' width={'10%'}/>
+        <Hypnosis color="#2b5877" width="200px" height="200px" duration="3s" />
+        <img src={logo} className='position-absolute' width={'7%'}/>
     </div>
   )
 }
